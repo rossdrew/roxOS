d5544ba474b45b7a3d0c9fdc9ddfbce15b642a2b
Ross Drew <thek1ng13@hotmail.com>
2015-10-24 12:06:47 +0100

    protected_mode - Some cleanup to help with debugging

--- lab/protected_mode/enter_protected_mode.asm
+++ lab/protected_mode/enter_protected_mode.asm
@@ -1,29 +1,29 @@
 [ bits 16]
 
+; ------------------------
 ; Switch to protected mode
+; ------------------------
 switch_to_pm :
  cli ; We must switch of interrupts until we have set -up the protected mode interrupt
 
- lgdt [ gdt_descriptor ] ; Load our global descriptor table , which defines
- ; the protected mode segments ( e.g. for code and data )
- mov eax, cr0 ; To make the switch to protected mode , we set
- or eax, 0x1 ; the first bit of CR0 , a control register
+ lgdt [ gdt_descriptor ]    ; Load GDT
+ mov eax, cr0               ; Swtich to protected mode = set first byte of CR0 (Control Register)
+ or eax, 0x1 
  mov cr0, eax
- jmp CODE_SEG:init_pm ; Make a far jump ( i.e. to a new segment ) to our 32- bit
- ; code. This also forces the CPU to flush its cache of
- ; pre - fetched and real - mode decoded instructions , which can
- ; cause problems.
+ jmp CODE_SEG:init_pm       ; Far jump to 32 bit code.  This forces CPU to flush its pieline.
  
 [ bits 32]
 
-; Initialise registers and the stack once in PM.
+; ---------------------------------
+; Initialise PM registers and stack
+; ---------------------------------
 init_pm :
- mov ax, DATA_SEG ; Now in PM , our old segments are meaningless ,
- mov ds, ax ; so we point our segment registers to the
- mov ss, ax ; data selector we defined in our GDT
+ mov ax, DATA_SEG           ; New Protected Mode data segments defined in GDT
+ mov ds, ax
+ mov ss, ax
  mov es, ax
  mov fs, ax
  mov gs, ax
- mov ebp, 0x90000 ; Update our stack position so it is right
- mov esp, ebp ; at the top of the free space.
- call BEGIN_PM ; Finally , call some well - known label
+ mov ebp, 0x90000           ; Move stack to top of free space
+ mov esp, ebp
+ call BEGIN_PM
